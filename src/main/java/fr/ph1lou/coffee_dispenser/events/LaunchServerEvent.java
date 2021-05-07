639093b72d080e6dbba43441a6d755629d3a3b1f
Ph1Lou <email@email.com>
2021-05-07 19:02:56 +0200

    fix

--- src/main/java/fr/ph1lou/coffee_dispenser/events/LaunchServerEvent.java
+++ src/main/java/fr/ph1lou/coffee_dispenser/events/LaunchServerEvent.java
@@ -26,7 +26,7 @@ public class LaunchServerEvent extends Event {
         this("FRA1",
                 "debian-10-x64",
                 "userdata.sh",
-                4,
+                4000,
                 uuid,
                 (id) -> {},
                 (id,error) -> {});
